69677920bb67e1cc2613fbbef23ac46699e83677
Josnelihurt Rodriguez <josnelihurt.rodriguez@arandasoft.com>
2018-10-07 19:06:37 -0500

    Adding schematic and fw ready to test

--- fw/servo_test/servo_test.ino
+++ fw/servo_test/servo_test.ino
@@ -1,4 +1,9 @@
 #include <SimpleServo.h>
+#include <SoftSerial.h>
+
+#define DEBUG_TX_RX_PIN         0 //Adjust here your Tx/Rx debug pin
+SoftSerial _dbgSerial(DEBUG_TX_RX_PIN, DEBUG_TX_RX_PIN, false); //true allows to connect to a regular RS232 without RS232 line driver
+ 
 #define LED_BUILTIN 1
 #define SW 2
 
@@ -11,15 +16,15 @@ enum states_t
   STATE_OPENED = 4,
   STATE_WAITING_TO_CLOSE = 5
 };
-int _valve_state = FULLY_CLOSED;
+int _valve_state = STATE_CLOSED;
 SimpleServo _servo; 
 int _servo_degree;    
-unsigned long _last_time;
-const unsigned long INTERVAL_TO_STABLE = 60000; // 
-const unsigned long INTERVAL_OPEN = 1000 * 60 * 2; // 2h
+unsigned long _last_time = 0;
+unsigned long _seconds_counter = 0;
+const unsigned long INTERVAL_SECONDS = 1000; 
 const int FULLY_OPEN = 84;
 const int FULLY_CLOSED = 10;
-const int STEP = 1;
+const int STEP = 3;
 
 bool _last_sw_state;
 void blink(unsigned char times){
@@ -31,17 +36,23 @@ void blink(unsigned char times){
   }
 }
 void update_state(int new_state){
-  //blink(new_state + 1);
+  _dbgSerial.print(F("\nNew state = "));
+  _dbgSerial.println(new_state);
   _valve_state = new_state;
   _last_time = millis();
+  _seconds_counter = 0;
 }
 void setup() {
+  _dbgSerial.begin(9600); 
+  _dbgSerial.println(F("\n****************"));
+  _dbgSerial.txMode(); //Before sending a message, switch to txMode
   pinMode(LED_BUILTIN, OUTPUT);  
   pinMode(SW, INPUT_PULLUP);  
   blink(3);
   _servo.attach(LED_BUILTIN);  // attaches the servo on pin 9 to the servo object
   _last_time = millis();
   _last_sw_state = false;
+  _seconds_counter = 0;
   update_state(STATE_CLOSED);
 }
 bool move_servo(int final_degree, int increment)
@@ -70,20 +81,26 @@ void update_state_from_input(){
     // State has changed
     _last_sw_state = sw_state;
     if(sw_state){
+      _dbgSerial.println(F("\nOPEN TRIGGERED"));
       update_state(STATE_OPENING);
     }else{
+      _dbgSerial.println(F("\nCLOSED TRIGGERED"));
       update_state(STATE_CLOSING); 
     }
   }
 }
+bool has_elapsed_second(){
+ bool result = ((unsigned long)(millis() - _last_time) >= INTERVAL_SECONDS);
+ if(result){
+  _dbgSerial.print(F("[s] "));  
+  _dbgSerial.println(_seconds_counter);
+  _last_time = millis();
+ }
+  return result;
+}
 void loop() {
+  int microseconds;
   delay(10);
-  /*if(digitalRead(SW)){
-    open_valve();
-  }else{
-   close_valve(); 
-  }
-  return;*/
   update_state_from_input();
   switch(_valve_state){
     case STATE_CLOSING:
@@ -95,8 +112,14 @@ void loop() {
     case STATE_CLOSED:
       _servo_degree = FULLY_CLOSED;
       _servo.write(_servo_degree);
-      if((millis() - _last_time) >= INTERVAL_TO_STABLE){
-        update_state(STATE_IDLE);
+      microseconds = _servo.readMicroseconds();
+      _servo.writeMicrosecondsMillis(microseconds,0);
+      if(has_elapsed_second()){
+        if(_seconds_counter++ > 5){
+          _dbgSerial.println(F("Valve fully closed"));
+          update_state(STATE_IDLE);
+        } 
+        
       }     
       break;
     case STATE_OPENING:
@@ -108,13 +131,20 @@ void loop() {
     case STATE_OPENED:
       _servo_degree = FULLY_OPEN;
       _servo.write(_servo_degree);
-      if((millis() - _last_time) >= INTERVAL_TO_STABLE){
-        update_state(STATE_WAITING_TO_CLOSE);
+      microseconds = _servo.readMicroseconds();
+      _servo.writeMicrosecondsMillis(microseconds,0);
+      if(has_elapsed_second()){
+        if(_seconds_counter++ > 5){
+          update_state(STATE_WAITING_TO_CLOSE);
+        }
       }
       break;
     case STATE_WAITING_TO_CLOSE:
-      if((millis() - _last_time) >= INTERVAL_OPEN){
-        update_state(STATE_CLOSING);
+      if(has_elapsed_second()){
+        if(_seconds_counter++ > 60*60){
+          _dbgSerial.println(F("\nTime up no more hot water!"));
+          update_state(STATE_CLOSING);
+        }       
       }
       break;
     case STATE_IDLE:
